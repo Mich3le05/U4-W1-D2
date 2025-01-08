2e1f56dfdd72c596df859dd89aee115e3f9af0e2
Michele Mandanici <michimandanici@tim.it>
2025-01-08 16:57:47 +0100

    esercizi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
     }
+        //Esercizio 1
+        // stringaPariDispari, che accetta una stringa e ritorna true se il numero di caratteri è pari e false se il numero di caratteri è dispari.
+        public static boolean stringaPariDispari (String str){
+            return str.length() % 2 == 0 ? true : false;
+        }
+
+        //        annoBisestile, che accetta un anno espresso come intero e ritorna true se esso è bisestile, false altrimenti.
+        public static boolean annoBisestile ( int anno){
+            if (anno % 400 == 0) {
+                return true;
+            } else if (anno % 4 == 0 && anno % 100 != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
 }
