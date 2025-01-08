def78a67211c48aa5eeadc1557ae8a9619f6b0f6
Michele Mandanici <michimandanici@tim.it>
2025-01-09 00:17:54 +0100

    edit

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
     }
-        //Esercizio 1
-        // stringaPariDispari, che accetta una stringa e ritorna true se il numero di caratteri è pari e false se il numero di caratteri è dispari.
-        public static boolean stringaPariDispari (String str){
-            return str.length() % 2 == 0 ? true : false;
-        }
 
-        //        annoBisestile, che accetta un anno espresso come intero e ritorna true se esso è bisestile, false altrimenti.
-        public static boolean annoBisestile ( int anno){
-            if (anno % 400 == 0) {
-                return true;
-            } else if (anno % 4 == 0 && anno % 100 != 0) {
-                return true;
-            } else {
-                return false;
-            }
+    //Esercizio 1
+    // stringaPariDispari, che accetta una stringa e ritorna true se il numero di caratteri è pari e false se il numero di caratteri è dispari.
+    public static boolean stringaPariDispari(String str) {
+        return str.length() % 2 == 0 ? true : false;
+    }
+
+    // annoBisestile, che accetta un anno espresso come intero e ritorna true se esso è bisestile, false altrimenti.
+    public static boolean annoBisestile(int anno) {
+        if (anno % 400 == 0) {
+            return true;
+        } else if (anno % 4 == 0 && anno % 100 != 0) {
+            return true;
+        } else {
+            return false;
         }
+    }
 
 }
--- src/Main2.java
+++ src/Main2.java
@@ -2,38 +2,40 @@ import java.util.Scanner;
 
 public class Main2 {
 
-	public static void main(String[] args) {
+    //	Scrivere un programma che chiede un intero in ingresso e lo stampa in lettere se il valore è compreso tra 0 e 3, altrimenti stampa un messaggio d'errore usando il costrutto switch.
+    public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		int test = scan.nextInt();
-		System.out.println(stampaNumero(test));
-		scan.close();
+        Scanner scan = new Scanner(System.in);
+        int test = scan.nextInt();
+        System.out.println(stampaNumero(test));
+        scan.close();
 
-	}
+    }
 
-	public static String stampaNumero(int num) {
-		String risultato;
-		switch (num) {
-		case 0: {
-			risultato = "zero";
-			break;
-		}
-		case 1: {
-			risultato = "uno";
-			break;
-		}
-		case 2: {
-			risultato = "due";
-			break;
-		}
-		case 3: {
-			risultato = "tre";
-			break;
-		}
-		default:
-			risultato = "Numero non compreso tra 0 e 3, impossibile convertire";
-		}
-		return risultato;
-	}
+    public static String stampaNumero(int num) {
+        String risultato;
+        switch (num) {
+        //Ogni case rappresenta un numero. Se il numero passato è pari al valore specificato nel case, il programma assegna alla variabile risultato il corrispondente valore testuale.
+            case 0: {
+                risultato = "zero";
+                break;
+            }
+            case 1: {
+                risultato = "uno";
+                break;
+            }
+            case 2: {
+                risultato = "due";
+                break;
+            }
+            case 3: {
+                risultato = "tre";
+                break;
+            }
+            default:
+                risultato = "Numero non compreso tra 0 e 3, impossibile convertire";
+        }
+        return risultato;
+    }
 
 }
